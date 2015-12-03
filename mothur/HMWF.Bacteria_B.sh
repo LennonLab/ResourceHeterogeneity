cee93c60039e6204f6d9f68b0cbde32708b7b94d
Mario Muscarella <mmuscare@indiana.edu>
2015-12-03 15:43:27 -0500

    renamed batchA.sh and fixed file name issue with other sh files

--- mothur/HMWF.Bacteria_B.sh
+++ mothur/HMWF.Bacteria_B.sh
@@ -4,7 +4,7 @@
 #PBS -M mmuscare@indiana.edu
 #PBS -m abe
 #PBS -j oe
-cd /N/dc2/projects/Lennon_Sequences/ResourceHeterogeneity
+cd /N/dc2/projects/Lennon_Sequences/2015_ResourceHeterogeneity
 module load gcc/4.9.2
 module load mothur/1.36.1
 mothur HMWF.Bacteria_B.Batch
